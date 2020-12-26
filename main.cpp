a4f217fd3d377bb9162d3b0fdec45771264fe297
Mikhail Karasikov <mike-karas@ya.ru>
2020-12-26 17:43:34 +0100

    cleanup, fixes of examples in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,47 +8,45 @@ int main(){
 	int n;
 	
 	/// Example 1 ///
+	{
+		n = 100;
+		ProgressBar bar1(n, "Example 1");
 
-	n = 100;
-	ProgressBar *bar1 = new ProgressBar(n, "Example 1");
-	
-	for(int i=0;i<=n;++i){
-		*bar1 += 1;
-        std::this_thread::sleep_for (std::chrono::milliseconds(10));
-    }
+		for (int i = 1; i <= n; ++i) {
+			bar1 += 1;
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	}
 
 	/// Example 2 ///
+	{
+		n = 1000;
+		ProgressBar bar2(n, "Example 2");
+		bar2.SetFrequencyUpdate(10);
+		bar2.SetStyle('|','-');
+		//bar2.SetStyle("\u2588", "-"); for linux
+		for (int i = 1; i <= n; ++i) {
+			bar2 += 1;
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+	}
 
-	n = 1000;
-	ProgressBar *bar2 = new ProgressBar(n, "Example 2");
-	bar2->SetFrequencyUpdate(10);
-	bar2->SetStyle('|','-');
-	//bar2->SetStyle("\u2588", "-"); for linux
-	
-	std::cout << std::endl;
-	for(int i=0;i<=n;++i){
-		*bar2 += 1;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-	
-	n = 5;
-	ProgressBar bar3(n);
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    // following tests exception error
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-    ++bar3;
+	ProgressBar bar3(5);
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	// following tests exception error
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	++bar3;
 
 	return 0;
 }
