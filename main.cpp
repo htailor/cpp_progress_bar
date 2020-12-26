f8dfa61ddb03fa8143013ee19ca072d71877c8a8
Atri Bhattacharya <badshah400@gmail.com>
2020-12-26 02:22:59 +0100

    Pass char instead of (char *) to SetStyle().
    This fixes compiling main.o without `-fpermissive`.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main(){
 	n = 1000;
 	ProgressBar *bar2 = new ProgressBar(n, "Example 2");
 	bar2->SetFrequencyUpdate(10);
-	bar2->SetStyle("|","-");
+	bar2->SetStyle('|','-');
 	//bar2->SetStyle("\u2588", "-"); for linux
 	
 	std::cout << std::endl;
